e90fae33019e9d899f78a4c9469a60660dffb706
0xhaz <28639279+0xhaz@users.noreply.github.com>
2023-06-09 02:12:29 +0800

    Added deploy scripts with filesync for ABI & address

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,6 +5,57 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+const fs = require("fs");
+const path = require("path");
+
+async function saveContractAddresses(tokenAddress, crowdsaleAddress) {
+  const abisDir = path.join(__dirname, "..", "src", "abis");
+  const tokenAbiPath = path.join(abisDir, "Token.json");
+  const crowdsaleAbiPath = path.join(abisDir, "Crowdsale.json");
+  const tokenAddressPath = path.join(abisDir, "TokenAddress.json");
+  const crowdsaleAddressPath = path.join(abisDir, "CrowdsaleAddress.json");
+  const configPath = path.join(__dirname, "..", "src", "config.json");
+
+  // Create the abis directory if it doesn't exist
+  if (!fs.existsSync(abisDir)) {
+    fs.mkdirSync(abisDir, { recursive: true });
+  }
+
+  // save the token address and abi
+  const tokenAbi = await hre.artifacts.readArtifact("DAPPToken");
+  fs.writeFileSync(tokenAbiPath, JSON.stringify(tokenAbi, null, 2));
+
+  // save the crowdsale address and abi
+  const crowdsaleAbi = await hre.artifacts.readArtifact("Crowdsale");
+  fs.writeFileSync(crowdsaleAbiPath, JSON.stringify(crowdsaleAbi, null, 2));
+
+  // save the token address
+  fs.writeFileSync(
+    tokenAddressPath,
+    JSON.stringify({ address: tokenAddress }, null, 2)
+  );
+
+  // save the crowdsale address
+  fs.writeFileSync(
+    crowdsaleAddressPath,
+    JSON.stringify({ address: crowdsaleAddress }, null, 2)
+  );
+
+  // save the token address and abi config file
+  const configData = {
+    31337: {
+      token: {
+        address: tokenAddress,
+      },
+      crowdsale: {
+        address: crowdsaleAddress,
+      },
+    },
+  };
+  fs.writeFileSync(configPath, JSON.stringify(configData, null, 2));
+
+  console.log("Contract addresses and ABIs saved successfully!");
+}
 
 async function main() {
   const Name = "DAPP Token";
@@ -26,7 +77,7 @@ async function main() {
   const Crowdsale = await hre.ethers.getContractFactory("Crowdsale");
   const Token = await hre.ethers.getContractFactory("DAPPToken");
 
-  let token = await Token.deploy(Name, Symbol, Max_Supply);
+  let token = await Token.deploy(Max_Supply);
   await token.deployed();
   console.log("Token deployed to:", token.address);
 
@@ -48,6 +99,8 @@ async function main() {
   );
   await transaction.wait();
   console.log("Transfered to Crowdsale");
+
+  await saveContractAddresses(token.address, crowdsale.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import { Container } from "react-bootstrap";
+
+import TOKEN_ABI from "./abis/Token.json";
+import CROWDSALE_ABI from "./abis/Crowdsale.json";
+import config from "./config.json";
 
 function App() {
+  const [provider, setProvider] = useState(null);
+  const [crowdsale, setCrowdsale] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [accountBalance, setAccountBalance] = useState(0);
+  const [maxTokens, setMaxTokens] = useState(0);
+  const [tokensSold, setTokensSold] = useState(0);
+  const [price, setPrice] = useState(0);
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+
+    const { chainId } = await provider.getNetwork();
+
+    const token = new ethers.Contract(
+      config[chainId].token.address,
+      TOKEN_ABI,
+      provider
+    );
+
+    const crowdsale = new ethers.Contract(
+      config[chainId].crowdsale.address,
+      CROWDSALE_ABI,
+      provider
+    );
+
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    const account = ethers.utils.getAddress(accounts[0]);
+    setAccount(account);
+
+    const accountBalance = ethers.utils.formatUnits(
+      await token.balanceOf(account),
+      18
+    );
+    setAccountBalance(accountBalance);
+
+    const price = await crowdsale.price();
+    setPrice(ethers.utils.formatUnits(price, 18));
+
+    const maxTokens = await crowdsale.maxTokens();
+    setMaxTokens(ethers.utils.formatUnits(maxTokens, 18));
+
+    const tokensSold = await crowdsale.tokensSold();
+    setTokensSold(ethers.utils.formatUnits(tokensSold, 18));
+
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (isLoading) {
+      loadBlockchainData();
+    }
+    setIsLoading(false);
+  }, [isLoading]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1 className="my-4 text-center">Introducing the DAPP Token!</h1>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <p className="text-center">
+            <strong>Current Price: </strong>
+            {price} ETH
+          </p>
+        </>
+      )}
+      <hr />
+      {account && (
+        <>
+          <p>{account}</p>
+        </>
+      )}
+    </Container>
   );
 }
 
